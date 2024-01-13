003358ff564b287fa2f47a7083d2909b3c4ea9ca
hahahai22 <72545759+hahahai22@users.noreply.github.com>
2024-01-13 21:13:14 +0800

    Update fun_call.cpp

--- fun_call.cpp
+++ fun_call.cpp
@@ -14,25 +14,12 @@ int multi(int a, int b)
   int c = a * b;
   return c;
 }
-class Point
-{
-private:
-  int x, y;
-public:
-  int getX(){return x;}
-  int getY(){return y;}
-};
+
 int main()
 {
   int a = 128;
   int b = 11;
   add(a, b);
   test();
-
-
-  int a = 12;
-  int b = 15;
-  max(a, b);
-  # 1 + 1 + 1
   return 0;
 }
